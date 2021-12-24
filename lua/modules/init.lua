a6e1f62020945c6f6db7a50b90642695c666a4cb
phelps <283861026@qq.com>
2021-12-24 17:19:41 +0800

    update

--- lua/modules/init.lua
+++ lua/modules/init.lua
@@ -1,3 +1,13 @@
+local command = vim.api.nvim_command
+local fn = vim.fn
+
+-- 安装 packer
+local packer_install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(packer_install_path)) > 0 then
+  command('!git clone https://github.com/wbthomason/packer.nvim '..packer_install_path)
+  command('packadd packer.nvim')
+end
+
 return require('packer').startup(function()
     -- Packer can manage itself
     use 'wbthomason/packer.nvim'
