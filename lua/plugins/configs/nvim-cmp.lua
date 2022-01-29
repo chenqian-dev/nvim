61015cfab6f2c64351585ec2d05f3dddf7a353c5
phelps <283861026@qq.com>
2022-01-29 14:22:45 +0800

    update

--- lua/plugins/configs/nvim-cmp.lua
+++ lua/plugins/configs/nvim-cmp.lua
@@ -11,8 +11,8 @@ M.setup = function()
     snippet = {
       -- REQUIRED - you must specify a snippet engine
       expand = function(args)
-        --  vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-        require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+         vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+        -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
         -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
         -- require'snippy'.expand_snippet(args.body) -- For `snippy` users.
       end,
@@ -42,8 +42,8 @@ M.setup = function()
     sources = cmp.config.sources(
     {
       { name = 'nvim_lsp' },
-      -- { name = 'vsnip' }, -- For vsnip users.
-      { name = 'luasnip' }, -- For luasnip users.
+      { name = 'vsnip' }, -- For vsnip users.
+      -- { name = 'luasnip' }, -- For luasnip users.
       -- { name = 'ultisnips' }, -- For ultisnips users.
       -- { name = 'snippy' }, -- For snippy users.
     },
